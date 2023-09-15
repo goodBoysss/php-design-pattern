71656ee6c73f5d1b0f22f9ec35aa929140786ea5
zhanglinxiao <zhanglinxiao@tianmtech.cn>
2023-09-15 16:59:02 +0800

    单例模式

--- Singleton Pattern（单例模式）/1.php
+++ Singleton Pattern（单例模式）/1.php
@@ -1,10 +1,13 @@
 <?php
+
 /**
  * 单例模式（Singleton Pattern）是一种创建型设计模式，用于确保一个类只有一个实例，并提供一个全局访问点来访问该实例
  */
 class Singleton
 {
     private static $instance;
+
+    private $time;
 
     private function __construct()
     {
@@ -19,5 +22,26 @@ class Singleton
         return self::$instance;
     }
 
-    // 其他方法...
+    public function setTime($time)
+    {
+        if (!isset($this->time)) {
+            $this->time = $time;
+        }
+
+    }
+
+    public function getTime()
+    {
+        return $this->time;
+    }
 }
+
+
+$s1 = Singleton::getInstance();
+$s1->setTime('9:00');
+var_dump($s1->getTime());
+
+$s2 = Singleton::getInstance();
+$s1->setTime('5:30');
+var_dump($s1->getTime());
+
